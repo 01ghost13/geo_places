bf7661e57c1792e038bf93c8ca6f44569fe30ef9
dmitry <orkich@gmail.com>
2018-04-10 13:07:15 +0300

    [fix] fixed bug with loading places; [add] more broad hashtypes

---
+++ hashTypes.py
@@ -0,0 +1,46 @@
+hashTypes = {
+                "еда": {
+                    "food", "cafe", "bar", "restaurant", "bakery"
+                },
+                "развлечения": {
+                    "amusement_park", "museum", "park", "zoo", "aquarium", "bowling_alley",
+                    "casino", "department_store", "liquor_store", "movie_theater", "painter", "spa"
+                },
+                "мода-красота": {
+                    "city_hall", "beauty_salon", "clothing_store", "hair_care", "health", "jewelry_store", "pharmacy",
+                    "shopping_mall", "spa"
+                },
+                "ночная жизнь": {
+                    "night_club", "jewelry_store", "liquor_store", "police",
+                    "food", "cafe", "bar", "restaurant"
+                },
+                "спорт": {
+                    "gym", "stadium", "bicycle_store"
+                },
+                "отношения": {
+                    "park", "zoo", "florist", "jewelry_store", "lodging", "movie_theater", "pharmacy",
+                    "food", "cafe", "bar", "restaurant", "shopping_mall"
+                },
+                "достопримечательности": {
+                    "museum", "jewelry_store", "hindu_temple", "mosque"
+                },
+                "работа": {
+                    "establishment", "city_hall", "car_dealer", "car_rental", "car_repair", "car_wash",
+                    "cemetery", "convenience_store", "courthouse", "dentist", "doctor", "embassy"
+                    "finance", "fire_station", "funeral_home", "furniture_store", "gas_station",
+                    "general_contractor", "grocery_or_supermarket", "home_goods_store", "lawyer", "liquor_store"
+                    "local_government_office", "painter", "pet_store", "pharmacy", "physiotherapist", "plumber",
+                    "police", "post_office", "real_estate_agency", "shopping_mall"
+                },
+                "учеба": {
+                    "school", "university"
+                },
+                "увлечения": {
+                    "art_gallery", "book_store", "campground", "hindu_temple", "library", "mosque",  "movie_rental",
+                    "painter"
+                },
+                "другое": {
+                    "airport", "atm", "bank", "bus_station", "church", "electrician",
+                    "electronics_store", "hardware_store", "laundry", "locksmith", "meal_delivery", "meal_takeaway"
+                }
+            }
