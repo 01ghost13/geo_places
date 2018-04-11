e337143f4bc8c039da62a0a2c72d81991c58923a
Unknown <orkich@gmail.com>
2018-04-11 10:51:07 +0300

    [chore] full broaded file

--- main.py
+++ main.py
@@ -4,7 +4,7 @@ from generator import Generator
 def run():
     gen = Generator('insta_geo.json', None)
     try:
-        gen.process(5)
+        gen.process(800)
     except Exception as e:
         print('Error has occurred!')
         print("Error: ", str(e))
